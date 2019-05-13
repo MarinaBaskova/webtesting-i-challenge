7680b050c7f011f2670871cc8ac79c4711517863
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-13 11:46:03 -0700

    added test for repair method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,28 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
+// a repair(item) method that accepts an item object
+// and returns a new item with the durability restored to 100.
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
+
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+	describe('repair method', () => {
+		it('should return an object with the durability restored to 100', () => {
+			expect(enhancer.repair({ name: 'sword', enhancement: 10, durability: 50 })).toEqual({
+				name: 'sword',
+				enhancement: 10,
+				durability: 100
+			});
+			expect(enhancer.repair({ name: 'arbalest', enhancement: 20, durability: 100 })).toEqual({
+				name: 'arbalest',
+				enhancement: 20,
+				durability: 100
+			});
+			expect(enhancer.repair({ name: 'gold sword', enhancement: 5, durability: 20 })).toEqual({
+				name: 'gold sword',
+				enhancement: 5,
+				durability: 100
+			});
+		});
+	});
+});
