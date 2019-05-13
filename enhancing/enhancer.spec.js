d4971477ead2c0b5f047a89e4460ca4221d70ca8
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-13 12:58:29 -0700

    added test for fail method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -38,7 +38,7 @@ describe('enhancer.js', () => {
 		});
 	});
 	describe('succeed method ', () => {
-		it('should return an object, if enhancement is 20, it will not be changed. Otherwise increases by 1', () => {
+		it('should return an object, if enhancement is equal 20, it will not be changed. Otherwise increases by 1', () => {
 			expect(enhancer.succeed(item1)).toEqual({
 				name: item1.name,
 				enhancement: 11,
@@ -48,6 +48,25 @@ describe('enhancer.js', () => {
 				name: item2.name,
 				enhancement: 20,
 				durability: item2.durability
+			});
+		});
+	});
+	describe('fail method', () => {
+		it('should return an object, with durability decreased by 5 if enchancement less then 15, else it decreases by 10. Should return enhancement decreased by 1, if it was greater then 16', () => {
+			expect(enhancer.fail(item1)).toEqual({
+				name: item1.name,
+				enhancement: item1.enhancement,
+				durability: 45
+			});
+			expect(enhancer.fail(item2)).toEqual({
+				name: item2.name,
+				enhancement: 19,
+				durability: 90
+			});
+			expect(enhancer.fail(item3)).toEqual({
+				name: item3.name,
+				enhancement: item3.enhancement,
+				durability: 15
 			});
 		});
 	});
